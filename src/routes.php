1caf260fadcd724011f86fb0aee2b0c892ef5f04
Saeed Vaziry <sa.vaziry@gmail.com>
2018-11-22 11:52:32 +0330

    fix routing

--- src/routes.php
+++ src/routes.php
@@ -4,11 +4,11 @@
 $router->any(config('laravel-swagger.routes.docs').'/{jsonFile?}', [
     'as' => 'laravel-swagger.docs',
     'middleware' => config('laravel-swagger.routes.middleware.docs', []),
-    'uses' => '\Cotlet\LaravelSwagger\Http\Controllers\SwaggerController@docs',
+    'uses' => '\SaeedVaziry\LaravelSwagger\Http\Controllers\SwaggerController@docs',
 ]);
 
 $router->get(config('laravel-swagger.routes.docs').'/asset/{asset}', [
     'as' => 'laravel-swagger.asset',
     'middleware' => config('laravel-swagger.routes.middleware.asset', []),
-    'uses' => '\Cotlet\LaravelSwagger\Http\Controllers\SwaggerAssetController@index',
+    'uses' => '\SaeedVaziry\LaravelSwagger\Http\Controllers\SwaggerAssetController@index',
 ]);
